c418218235440bafba56ee74b7eb56f7f6b3c077
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2021-09-16 08:36:36 -0400

    ExploratoryFigures

--- MRattempt.R
+++ MRattempt.R
@@ -4,16 +4,39 @@ metadat <- escalc(measure = "SMD",
                   sd1i = Manip_SD, sd2i = Control_SD,
                   n1i = N, n2i = N, 
                   slab = paste(Study_number, Author, Study_midyear),
-                  data = meta_df)
+                  data = meta_time5)
 
-metadat %>%
+meta_time4 %>%
   filter(metadat$yi < 45,
-         Percent_control < 350) -> metadat2
+         Percent_control < 350) -> meta_time5
+
+meta_time3 <- read.csv("meta_time.csv")
+
+meta_time3 %>%
+  distinct() -> meta_time4
 
 res <- rma(yi, vi, 
-           mods= ~Percent_control + Latitude - 1,
-           data = metadat2)
+           mods= ~Percent_control + Latitude + Year - 1,
+           data = metadat)
 res
 
-ggplot(metadat2, aes(Percent_control, yi)) +
-  geom_point(size = 4, na.rm = TRUE) + geom_smooth(method = "lm")
+
+library(ggpmisc)
+my.formula <- y ~ x
+
+ggplot(metadat2, aes(Year, yi)) +
+  geom_point(size = 4, na.rm = TRUE) +
+  geom_smooth(method = "lm", formula = my.formula) +
+  stat_poly_eq(formula = my.formula, 
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
+               parse = TRUE) #+ facet_grid(Manipulation~.)
+
+library(meta)
+library(esc)
+
+altmetadata <- esc_mean_sd(grp1m = meta_time4$Manip_Resp, grp2m = meta_time4$Control_Resp, 
+                            grp1sd = meta_time4$Manip_SD, grp2sd = meta_time4$Control_SD,
+                              grp1n = meta_time4$N, grp2n = meta_time4$N)
+
+
+soilresp_time <- metareg(altmetadata, Year)
--- MultiYearStudies.R
+++ MultiYearStudies.R
@@ -8,3 +8,30 @@ ggplot(meta_time, aes(Study_midyear, Manip_Resp-Control_Resp, color = Author)) +
   geom_point(na.rm = TRUE) + geom_line() +
   facet_grid(Ecosystem_type~Manipulation) + theme(legend.position = "bottom")
 
+write.csv(meta_time, "meta_time.csv")
+
+meta_time2 <- read.csv("meta_time.csv")
+
+
+length(unique(meta_df$Study_number))
+length(unique(meta_time$Study_number))
+length(unique(meta_time$Author))
+
+metadat <- escalc(measure = "RR",
+                  m1i = Manip_Resp, m2i = Control_Resp, 
+                  sd1i = Manip_SD, sd2i = Control_SD,
+                  n1i = N, n2i = N, 
+                  slab = paste(Study_number, Author, Study_midyear),
+                  data = meta_time2)
+
+metadat %>%
+  filter(metadat$yi < 45,
+         Percent_control < 350) -> metadat2
+
+res <- rma(yi, vi, 
+           mods= ~Percent_control + Latitude - 1,
+           data = metadat2)
+res
+
+ggplot(metadat2, aes(Year, yi)) + 
+  geom_point(size = 4, na.rm = TRUE) + geom_smooth(method = "lm") + facet_grid(Manipulation~.)
