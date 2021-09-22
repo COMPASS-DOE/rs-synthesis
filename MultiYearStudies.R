0a5b8fa469343a2c7d79f68143635cc2f497ba83
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2021-09-22 17:24:21 -0400

    misc.

--- MultiYearStudies.R
+++ MultiYearStudies.R
@@ -1,43 +1,41 @@
 
-
+#filter meta_df to get studies with more than one year
 meta_df %>%
   group_by(Study_number) %>%
   filter(length(unique(Study_midyear)) > 1 ) -> meta_time
 
+#summary plot
 ggplot(meta_time, aes(Study_midyear, Manip_Resp-Control_Resp, color = Author)) + 
   geom_point(na.rm = TRUE) + geom_line() +
   facet_grid(Ecosystem_type~Manipulation) + theme(legend.position = "bottom")
 
-write.csv(meta_time, "meta_time.csv")
-meta_time2 <- read.csv("meta_time.csv")
-
-
-length(unique(meta_df$Study_number))
-length(unique(meta_time$Study_number))
-length(unique(meta_time$Author))
-
-
-#what is actually getting dropped here?
-#what is going on with the Fay studies, see control SD
-meta_time2 %>%
-  distinct() -> meta_time3
-
+#compute effect sizes
 metadat <- escalc(measure = "SMD",
                   m1i = Manip_Resp, m2i = Control_Resp, 
                   sd1i = Manip_SD, sd2i = Control_SD,
                   n1i = N, n2i = N, 
                   slab = paste(Study_number, Author, Study_midyear),
-                  data = meta_time2)
-
-metadat %>%
-  filter(metadat$yi < 45,
-         Percent_control < 350) -> metadat2
+                  data = meta_time)
 
 #also need to think about filtering outliers
+metadat %>%
+  filter(metadat$yi < 45) -> metadat2
+
+metadat2 %>%
+  group_by(Study_number) %>%
+  filter(length(unique(Study_midyear)) > 1 ) -> metadat_time
+
+ggplot(metadat_time, aes(YearOfStudy, yi, color = Ecosystem_type, group = Percent_control)) + 
+  geom_point(na.rm = TRUE) + geom_line() +
+  facet_grid(Ecosystem_type~Manipulation) + theme(legend.position = "bottom")
+
+
+
+
 
 res <- rma(yi, vi, 
-           mods= ~Year + Percent_control - 1,
-           data = metadat2)
+           mods= ~YearOfStudy + Percent_control - 1,
+           data = metadat_time)
 res
 
 ggplot(metadat, aes(Year, yi)) + 
@@ -54,8 +52,8 @@ upper_ci <- function(mean, se, n, conf_level = 0.95){
   upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
 }
 
-test <- metadat %>%
-  group_by(Manipulation, Year) %>%
+test <- metadat_time %>%
+  group_by(Manipulation, YearOfStudy) %>%
   summarise(average = mean(yi, na.rm = TRUE),
             ssd = sd(yi, na.rm = TRUE),
             count = n()) %>%
@@ -63,7 +61,7 @@ test <- metadat %>%
          lower_ci = lower_ci(average, se, count),
          upper_ci = upper_ci(average, se, count))
 
-ggplot(test, aes(average, Year, label = count)) + 
+ggplot(test, aes(average, YearOfStudy, label = count)) + 
   geom_vline(xintercept = 0) +
   geom_errorbarh(aes(xmax = upper_ci, xmin = lower_ci,
                      na.rm = TRUE)) +
