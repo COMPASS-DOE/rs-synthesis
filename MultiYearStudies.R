6460e6697a81a048f18953aff6670c09e103bc4f
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2021-10-13 17:02:14 -0400

    Major Code Clean-Up
    Deleted many thanks that didn't make sense post QA-QC from bug squashing merge, updated some graphs

--- MultiYearStudies.R
+++ MultiYearStudies.R
@@ -1,46 +1,45 @@
 
-#filter meta_df to get studies with more than one year
-meta_df %>%
-  group_by(Study_number) %>%
-  filter(length(unique(Study_midyear)) > 1 ) -> meta_time
 
-#summary plot
-ggplot(meta_time, aes(Study_midyear, Manip_Resp-Control_Resp, color = Author)) + 
-  geom_point(na.rm = TRUE) + geom_line() +
-  facet_grid(Ecosystem_type~Manipulation) + theme(legend.position = "bottom")
+#duration
+meta_df %>%
+  group_by(Study_number, Manipulation_level, Ecosystem_type) %>%
+  mutate(Duration = Study_midyear - min(Study_midyear)) -> meta_time
+
 
 #compute effect sizes
-metadat <- escalc(measure = "SMD",
+metadat <- escalc(measure = "ROM",
                   m1i = Manip_Resp, m2i = Control_Resp, 
                   sd1i = Manip_SD, sd2i = Control_SD,
                   n1i = N, n2i = N, 
                   slab = paste(Study_number, Author, Study_midyear),
                   data = meta_time)
 
-#also need to think about filtering outliers
-metadat %>%
-  filter(metadat$yi < 45) -> metadat2
-
-metadat2 %>%
-  group_by(Study_number) %>%
-  filter(length(unique(Study_midyear)) > 1 ) -> metadat_time
-
-ggplot(metadat_time, aes(YearOfStudy, yi, color = Ecosystem_type, group = Percent_control)) + 
-  geom_point(na.rm = TRUE) + geom_line() +
-  facet_grid(Ecosystem_type~Manipulation) + theme(legend.position = "bottom")
-
-
-
-
-
+#conduct meta-analysis
 res <- rma(yi, vi, 
-           mods= ~YearOfStudy + Percent_control - 1,
-           data = metadat_time)
+           mods= ~Ecosystem_type + Duration + Percent_control - 1,
+           data = metadat)
 res
 
-ggplot(metadat, aes(Year, yi)) + 
+
+#linear relationship between effect size and duration
+ggplot(metadat, aes(Duration, yi)) + 
   geom_point(size = 4, na.rm = TRUE) + geom_smooth(method = "lm") #+ facet_grid(Manipulation~.)
 
+#summary plot WORK IN PROGRESS
+#would be great to have a combination factor that would make each line connect only one set of points
+ggplot(metadat, aes(Duration, yi, color = Author)) + 
+  geom_point(na.rm = TRUE) + geom_line() +
+  facet_grid(Manipulation~.) + theme(legend.position = "bottom")
+
+#Studies that last @least four years
+metadat %>%
+  group_by(Study_number) %>%
+  filter(length(unique(Study_midyear)) > 3 ) -> md4year
+
+
+ggplot(md4year, aes(Duration, yi, color = Author)) + 
+  geom_point(na.rm = TRUE) + geom_line() +
+  facet_grid(Manipulation~.) + theme(legend.position = "bottom")
 
 #summarize mean effect size, 95%CI, and count by year of study (and manipulation type)
 #metadat
@@ -52,8 +51,8 @@ upper_ci <- function(mean, se, n, conf_level = 0.95){
   upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
 }
 
-test <- metadat_time %>%
-  group_by(Manipulation, YearOfStudy) %>%
+test <- metadat %>%
+  group_by(Manipulation, Duration) %>%
   summarise(average = mean(yi, na.rm = TRUE),
             ssd = sd(yi, na.rm = TRUE),
             count = n()) %>%
@@ -61,7 +60,7 @@ test <- metadat_time %>%
          lower_ci = lower_ci(average, se, count),
          upper_ci = upper_ci(average, se, count))
 
-ggplot(test, aes(average, YearOfStudy, label = count)) + 
+ggplot(test, aes(average, Duration, label = count)) + 
   geom_vline(xintercept = 0) +
   geom_errorbarh(aes(xmax = upper_ci, xmin = lower_ci,
                      na.rm = TRUE)) +
