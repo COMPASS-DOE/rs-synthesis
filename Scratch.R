37152a556ccf1d361fd50c89a41b686e0d0987ef
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2021-12-06 13:38:19 -0500

    graph tweaks 203

--- Scratch.R
+++ Scratch.R
@@ -21,46 +21,52 @@ animateSD <- animate(SD)
 
 
 plyr::count(metadat[metadat$Manipulation == "Irrigation",]$Ecosystem_type)
-Ecoi_ss <- c("13","36","83","7","5")
+Ecoi_ss <- c("36","5","7","83","13")
 
 Foresti <- forest(Coef_irrigation[1:5,]$estimate,
                   ci.lb = Coef_irrigation[1:5,]$ci.lb,
                   ci.ub = Coef_irrigation[1:5,]$ci.ub,
                   annotate = TRUE,
                   xlab = "ln(Response Ratio)",
-                  slab = c("Desert", "Forest", "Grassland",
-                           "Savanna", "Shrubland"),
-                  
-                  cex = 1,
-                  digits = 2)
+                  slab = c("Forest", "Shrubland",
+                           "Savanna", "Grassland", "Desert"),
+                  cex = 1.25,
+                  cex.lab = 2,
+                  cex.main = 3,
+                  digits = 2,
+                  lwd = 3
+                  )
 
-text(-.25, rev(seq(5:1)), Ecoi_ss, cex = 1) # Code to write sample size of sub-groups on graph
-op <- par(cex=1, font=2) # Set up font for rest of graph (just the headers of the graph remain), to make bold headings, set font=2
+text(-.14, rev(seq(5:1)), Ecoi_ss, cex = 1.5) # Code to write sample size of sub-groups on graph
+op <- par(cex=2, font=2) # Set up font for rest of graph (just the headers of the graph remain), to make bold headings, set font=2
 text(-.5, 6.2, "Ecosystem") # For this code, enter x-position of text, then y-position. You may have to experiment a bit.
-text(-.25, 6.2, "Sample Size")
-text(0.7, 6.2, "ln(Response Ratio) [95% CI]")
-text(0,7, "Increased Precipitation Studies")
+text(-.14, 6.2, "Sample Size")
+text(0.85, 6.2, "ln(RR) [95% CI]")
+text(0,7, "Increased Precipitation Studies", cex = 1.2)
 
 plyr::count(metadat[metadat$Manipulation == "Drought",]$Ecosystem_type)
-Ecod_ss <- c("7","38","55","1","27","2")
+Ecod_ss <- c("2","38","27","1","55","7")
 
 Forestd <- forest(Coef_drought[1:6,]$estimate,
                   ci.lb = Coef_drought[1:6,]$ci.lb,
                   ci.ub = Coef_drought[1:6,]$ci.ub,
                   annotate = TRUE,
                   xlab = "ln(Response Ratio)",
-                  slab = c("Desert", "Forest", "Grassland",
-                           "Savanna", "Shrubland", "Wetland"),
-                  
-                  cex = 1,
-                  digits = 2)
+                  slab = c("Wetland", "Forest", "Shrubland",
+                           "Savanna", "Grassland", "Wetland"),
+                  cex = 1.25,
+                  cex.lab = 2,
+                  cex.main = 3,
+                  digits = 2,
+                  lwd = 3
+)
 
-text(-1.2, rev(seq(6:1)), Ecod_ss, cex = 1) # Code to write sample size of sub-groups on graph
-op <- par(cex=1, font=2) # Set up font for rest of graph (just the headers of the graph remain), to make bold headings, set font=2
+text(-1.2, rev(seq(6:1)), Ecod_ss, cex = 1.5) # Code to write sample size of sub-groups on graph
+op <- par(cex=2, font=2) # Set up font for rest of graph (just the headers of the graph remain), to make bold headings, set font=2
 text(-2.25, 7.2, "Ecosystem") # For this code, enter x-position of text, then y-position. You may have to experiment a bit.
 text(-1.2, 7.2, "Sample Size")
-text(1.3, 7.2, "ln(Response Ratio) [95% CI]")
-text(0,8.2, "Decreased Precipitation Studies")
+text(1.3, 7.2, "ln(RR) [95% CI]")
+text(0,8, "Decreased Precipitation Studies", cex = 1.2)
 
 
 library(ggpmisc)
