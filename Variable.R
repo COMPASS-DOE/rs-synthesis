cd09523a9bd6315540c78c000ef3e58b748c1300
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2022-02-20 14:25:59 -0500

    dependent Forest plots

--- Variable.R
+++ Variable.R
@@ -17,6 +17,25 @@ metadat_imodel_var <- rma.mv(yi, vi,
                              slab = Ecosystem_type,
 )
 summary(metadat_imodel_var)
+Coef_ivar <- coef(summary(metadat_imodel_var))
+Forestivar <- forest(Coef_ivar[1:3, ]$estimate,
+                  ci.lb = Coef_ivar[1:3, ]$ci.lb,
+                  ci.ub = Coef_ivar[1:3, ]$ci.ub,
+                  annotate = TRUE,
+                  slab = c(
+                    "Rh_annual", "Rs_annual",
+                    "Rs_growingseason"
+                  ),
+                  cex = 1,
+                  cex.lab = 1,
+                  cex.main = 2,
+                  digits = 1,
+                  lwd = 2
+)
+#irrigation studies
+#there are differences in the statistical significance of the RR's
+#BUT they are not statistically different from each other
+#evidence is 95% CI overlap
 
 metadat_dmodel_var <- rma.mv(yi, vi,
                          random = ~ 1 | Study_number,
@@ -26,8 +45,25 @@ metadat_dmodel_var <- rma.mv(yi, vi,
                          slab = Ecosystem_type,
 )
 summary(metadat_dmodel_var)
-
-
+Coef_dvar <- coef(summary(metadat_dmodel_var))
+Forestdvar <- forest(Coef_dvar[1:3, ]$estimate,
+                     ci.lb = Coef_dvar[1:3, ]$ci.lb,
+                     ci.ub = Coef_dvar[1:3, ]$ci.ub,
+                     annotate = TRUE,
+                     slab = c(
+                       "Rh_annual", "Rs_annual",
+                       "Rs_growingseason"
+                     ),
+                     cex = 1,
+                     cex.lab = 1,
+                     cex.main = 2,
+                     digits = 1,
+                     lwd = 2
+)
+#drought studies
+#here are no differences in the statistical significance of the RR's
+#ANDthey are not statistically different from each other
+#evidence is 95% CI overlap
 
 
 
